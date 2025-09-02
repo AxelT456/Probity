3601895c0616fd33029a4fbb949e906de958282f
Axel Tapia <axurmen214@gmail.com>
2025-09-02 07:42:36 -0600

    Configuración de Comunicación con el Front

--- Probity/Probity/settings.py
+++ Probity/Probity/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.formulas.apps.FormulasConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # La dirección de tu app de Vue
+    "http://127.0.0.1:5173",
 ]
 
 ROOT_URLCONF = 'Probity.urls'
